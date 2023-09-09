6b8579e07145c711b65f2c79f11ffa8fce2996eb
Wangyelan <118416712+Wangyelan@users.noreply.github.com>
2023-09-09 16:29:38 +0800

    Add files via upload

---
+++ galigali/src/test/java/com/gali/galigali/main/java/com/gali/galigali/mapper/LogMapper.java
@@ -0,0 +1,10 @@
+package com.gali.galigali.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gali.galigali.dao.Log;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LogMapper extends BaseMapper<Log> {
+
+}
